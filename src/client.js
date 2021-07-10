e05f99f4e3c3b8838d3bead77d669549f6e27966
Diego Mais <diegomais.dev@gmail.com>
2021-07-10 18:45:50 -0300

    ♻️ add promisify

--- src/client.js
+++ src/client.js
@@ -7,16 +7,32 @@ const protoObject = protoLoader.loadSync(
 )
 const NotesDefinition = grpc.loadPackageDefinition(protoObject)
 
+function promisify(client) {
+  for (let method in client) {
+    client[`${method}Async`] = (parameters) => {
+      return new Promise((resolve, reject) => {
+        client[method](parameters, (err, response) => {
+          if (err) reject(err)
+          resolve(response)
+        })
+      })
+    }
+  }
+}
+
 const client = new NotesDefinition.NoteService(
   'localhost:50051',
   grpc.credentials.createInsecure()
 )
+promisify(client)
 
+client.listAsync({}).then(console.log)
 client.list({}, (err, notes) => {
   if (err) throw err
   console.log(notes)
 })
 
+client.findAsync({ id: 2 }).then(console.log).catch(console.error)
 client.find({ id: 2 }, (err, { note }) => {
   if (err) throw console.error(err.details)
   if (!note) return console.error('Not found')
